1a61f7b835b33ad07d413df488c0808aae68adfb
Michael Mikhalevich <michael.mikhalevich@gmail.com>
2025-10-13 23:26:57 +0300

    generate test data payload

--- internal/app/httpapi/internal/handler/generate_test_data.go
+++ internal/app/httpapi/internal/handler/generate_test_data.go
@@ -1,16 +1,31 @@
 package handler
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
 )
 
-const (
-	defaultTestDataCount = 10000
-)
+type GenerateTestDataRequest struct {
+	RowsCount int `json:"rows_count"`
+}
 
 func (h *Handler) GenerateTestData(w http.ResponseWriter, r *http.Request) {
-	if err := h.scoreGenerator.Generate(r.Context(), defaultTestDataCount); err != nil {
+	var req GenerateTestDataRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		slog.Error("json decode", slog.String("error", err.Error()))
+		http.Error(w, "json decode erorr", http.StatusBadRequest)
+
+		return
+	}
+
+	if req.RowsCount <= 0 {
+		http.Error(w, "invalid rows count", http.StatusBadRequest)
+
+		return
+	}
+
+	if err := h.scoreGenerator.Generate(r.Context(), req.RowsCount); err != nil {
 		slog.Error("score generate error", slog.String("error", err.Error()))
 		http.Error(w, "generate data error", http.StatusInternalServerError)
 
